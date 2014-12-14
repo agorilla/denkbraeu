22a46f3aefd5c25a229ee8cf0a3e1471c5c652c9
Simon Koch <agorilla@me.com>
2014-12-15 00:22:55 +0100

    Responsive images
    Optimized images for every viewport for the teaser-hero image

--- dev/Gruntfile.js
+++ dev/Gruntfile.js
@@ -20,7 +20,91 @@ module.exports = function (grunt) {
 			options: {
 				// available options:
 				// https://github.com/nDmitry/grunt-autoprefixer#options
-				browsers: [ '> 1%', 'last 2 versions' ]
+				browsers: ['> 1%', 'last 2 versions']
+			}
+		},
+
+		// Responsive images
+		responsive_images: {
+			dev: {
+				options: {
+					sizes: [{
+						name: 'small',
+						width: 420,
+						quality: 90
+					}, {
+						name: 'small-retina',
+						width: 840,
+						quality: 50
+					}, , {
+						name: 'small-retina-webp',
+						width: 840,
+						quality: 100
+					}, {
+						name: 'medium',
+						width: 800,
+						quality: 90
+					}, {
+						name: 'medium-retina',
+						width: 1600,
+						quality: 50
+					}, {
+						name: 'medium-retina-webp',
+						width: 1600,
+						quality: 100
+					}, {
+						name: 'large',
+						width: 1280,
+						quality: 90
+					}, {
+						name: 'large-retina',
+						width: 2560,
+						quality: 45
+					}, {
+						name: 'large-retina-webp',
+						width: 2560,
+						quality: 100
+					}],
+
+					engine: 'im',
+					newFilesOnly: false
+				},
+				files: [{
+					expand: true,
+					cwd: 'img',
+					src: ['**/*.jpg'],
+					dest: 'img-temp'
+				}]
+			}
+		},
+
+
+		// WebP compression
+		cwebp: {
+			normal: {
+				options: {
+					arguments: ['-q', 85],
+					concurrency: 20
+				},
+				files: [
+					{src: ['img-temp/**/*.jpg']}
+				]
+			},
+			retina: {
+				options: {
+					arguments: ['-q', 65],
+					concurrency: 20
+				},
+				files: [
+					{src: ['img-temp/**/*-retina-webp.jpg']}
+				]
+			}
+		},
+
+		// Lossless image optimization
+		imageoptim: {
+			dev: {
+				src: ['img-temp']
 			}
 		},
 
@@ -28,9 +112,9 @@ module.exports = function (grunt) {
 		svgmin: {
 			options: {
 				plugins: [
-					{ removeViewBox: false },
-					{ removeUselessStrokeAndFill: true },
-					{ cleanupIDs: false }
+					{removeViewBox: false},
+					{removeUselessStrokeAndFill: true},
+					{cleanupIDs: false}
 				]
 			},
 			dist: {
@@ -45,23 +129,64 @@ module.exports = function (grunt) {
 				]
 			}
 		},
-		
+
+		// Clean tasks
+		clean: {
+			images: {
+				src: ['img-temp']
+			},
+			webp: {
+				src: ['img-temp/**/*-webp.jpg', 'img-temp/**/*-retina.jpg.webp']
+			}
+		},
+
+		// Copy tasks
+		copy: {
+			images: {
+				files: [{
+					expand: true,
+					cwd: 'img-temp',
+					src: ['**'],
+					dest: '../dist/img/',
+
+					rename: function (dest, src) {
+						return dest + src.replace('-webp.jpg.webp', '.webp').replace('.jpg.webp', '.webp');
+					}
+				}]
+			}
+		},
+
+		// Watch tasks
 		watch: {
-		    files: ['sass/**/*.scss'],
-			tasks: ['sass', 'autoprefixer']
+			sass: {
+				files: ['sass/**/*.scss'],
+				tasks: ['dev-sass']
+			},
+			images: {
+				files: ['img/**/*.jpg'],
+				tasks: ['image']
+			},
+			svg: {
+				files: ['img/**/*.svg'],
+				tasks: ['svg']
+			}
 		}
 
 	});
 
 	// Load Grunt tasks automatically
-	require('load-grunt-tasks')(grunt, { scope: 'devDependencies' });
+	require('load-grunt-tasks')(grunt, {scope: 'devDependencies'});
+
+	grunt.registerTask('dev-sass', ['sass', 'autoprefixer']);
+	grunt.registerTask('image', ['clean:images', 'responsive_images', 'cwebp', 'clean:webp', 'imageoptim', 'copy:images', 'clean:images']);
+	grunt.registerTask('svg', ['svgmin']);
 
 	// Default task(s).
 	// ORDER IS IMPORTANT
 	grunt.registerTask('default', [
-		'sass',
-		'autoprefixer',
-		'svgmin'
+		'dev-sass',
+		'svg'
 	]);
+
 
 };
